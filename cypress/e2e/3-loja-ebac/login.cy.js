c843b3d8c0273bf5799acce70d08335942e061f9
Ashfarell <ashfarell@gmail.com>
2025-10-15 13:12:26 -0300

    atualização c/ BeforeE e AfterE

--- cypress/e2e/3-loja-ebac/login.cy.js
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -1,19 +1,25 @@
 /// <reference types="cypress" />
 
 describe('Funcionalidade: Login', () => {
-  
-    it('Deve fazer login com sucesso', () => {
+
+    beforeEach(() => {
         cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+    });
+
+    afterEach(() => {
+        cy.screenshot
+    });
+ 
+    it.only('Deve fazer login com sucesso', () => {
+        //cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
        cy.get('#username').type('jean.teste@teste.com.br')
        cy.get('#password').type('Jlb@1988')
         cy.get('.woocommerce-form > .button').click()
-
-
          cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain' , 'Olá, jean.teste (não é jean.teste? Sair)')
     })
 
     it('Deve exibir uma mensagem de erro ao inserir usuário inválido', () => {
-         cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+         //cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
        cy.get('#username').type('jean.teste@teste.com.')
        cy.get('#password').type('Jlb@1988')
         cy.get('.woocommerce-form > .button').click()
@@ -23,12 +29,13 @@ describe('Funcionalidade: Login', () => {
            
     })
 
-it.only('Deve exibir mensagem de erro de senha', () => {
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
-       cy.get('#username').type('jean.teste@teste.com.')
-       cy.get('#password').type('Jlb@1988')
+it('Deve exibir mensagem de erro de senha', () => {
+        //cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+       cy.get('#username').type('jean.teste@teste.com.br')
+       cy.get('#password').type('Jlb@')
         cy.get('.woocommerce-form > .button').click()
-        cy.get('.woocommerce-error > li').should('contain' , 'O usuário jean.teste@teste.com. não está registrado neste site. ')
+        cy.get('.woocommerce-error > li').should('contain' , 'A senha fornecida para o e-mail jean.teste@teste.com.br está incorreta')
+        cy.get ('.woocommerce-error').should('exist')
 
 });
 
