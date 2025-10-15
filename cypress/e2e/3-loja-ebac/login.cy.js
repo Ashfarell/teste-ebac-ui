0f32cd71a9dddf5422432d53ef9d26f936005e4b
Ashfarell <ashfarell@gmail.com>
2025-10-15 12:32:08 -0300

    atualização código

--- cypress/e2e/3-loja-ebac/login.cy.js
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -12,7 +12,7 @@ describe('Funcionalidade: Login', () => {
          cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain' , 'Olá, jean.teste (não é jean.teste? Sair)')
     })
 
-    it.only('Deve exibir uma mensagem de erro ao inserir usuário inválido', () => {
+    it('Deve exibir uma mensagem de erro ao inserir usuário inválido', () => {
          cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
        cy.get('#username').type('jean.teste@teste.com.')
        cy.get('#password').type('Jlb@1988')
@@ -20,8 +20,17 @@ describe('Funcionalidade: Login', () => {
 
         //cy.get('.woocommerce-error').should('contain' , 'O usuário jean.teste@teste.com. não está registrado neste site. ')
         cy.get('.woocommerce-error').should('exist')
+           
+    })
 
-        
-    });
+it.only('Deve exibir mensagem de erro de senha', () => {
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+       cy.get('#username').type('jean.teste@teste.com.')
+       cy.get('#password').type('Jlb@1988')
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-error > li').should('contain' , 'O usuário jean.teste@teste.com. não está registrado neste site. ')
+
+});
+
 
 })
