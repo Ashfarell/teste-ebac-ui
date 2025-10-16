464a738e6a06cf294f456459f512b527e4059158
Ashfarell <ashfarell@gmail.com>
2025-10-15 22:25:32 -0300

    fake + variáveis

--- cypress/e2e/3-loja-ebac/cadastro.cy.js
+++ cypress/e2e/3-loja-ebac/cadastro.cy.js
@@ -5,28 +5,41 @@ describe('Funcionalidade: cadastro', () => {
 
     before(() => {
         cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
-        })
-
-  after(() => {
-        cy.screenshot
-    });
+        });
 
     it('Deve completar o cadastro com sucesso', () => {
     cy.get('#reg_email').type(faker.internet.email())
     cy.get('#reg_password').type(faker.internet.password())
     cy.get(':nth-child(4) > .button').click()
-
+  //cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain' , 'Olá,')
   cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('exist')
-
 cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a').click()
-
 cy.get('#account_first_name').type(faker.person.firstName())
+//cy.screenshot()
 cy.get('#account_last_name').type(faker.person.lastName())
-
 cy.wait(5000)
-
 cy.get('.woocommerce-Button').click()
-
+cy.get('.woocommerce-message').should('contain' , 'Detalhes da conta modificados com sucesso.')
 });
+
+ it.only('Deve completar o cadastro com sucesso usando variáveis', () => {
+        var email = faker.internet.email()
+        var nome = faker.person.firstName()
+        var sobrenome = faker.person.lastName()
+
+cy.get('#reg_email').type(email)
+ cy.get('#reg_password').type(faker.internet.password())
+ cy.get(':nth-child(4) > .button').click()
+ //cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain' , 'Olá,')
+ cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('exist')
+cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a').click()
+cy.get('#account_first_name').type(nome)
+//cy.screenshot()
+cy.get('#account_last_name').type(sobrenome)
+cy.wait(5000)
+cy.get('.woocommerce-Button').click()
+cy.get('.woocommerce-message').should('contain' , 'Detalhes da conta modificados com sucesso.')
+});
+
 
 })
