0e56bf97be5946bc07ba38b1d9ecd8dca5006f0b
Ashfarell <ashfarell@gmail.com>
2025-10-21 19:23:31 -0300

    atualização produtos c/ Método

--- cypress/e2e/3-loja-ebac/produtos.cy.js
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -1,23 +1,25 @@
 // <reference types="cypress" />
+import produtosPage from "../../support/page.objetcs/produtos.page";
 
 describe('Funcionalidade: Produtos', () => {
 
 beforeEach(() => {
-    //cy.visit('http://lojaebac.ebaconline.art.br/produtos/') forma inicial
-    cy.visit('produtos')   //forma com baseUrl no cypress.config.js
+    //cy.visit('http://lojaebac.ebaconline.art.br/produtos/')   forma inicial
+    //cy.visit('produtos')   //                                 forma com baseUrl no cypress.config.js
+    produtosPage.visitarUrl()
 });
 
+    it.only('Deve buscar um produto com sucesso', () => {  
+        produtosPage.buscarProduto('Atlas Fitness Tank');
+        cy.get('.product_title').should('contain', 'Atlas Fitness Tank');
+        cy.get('.product_title').should('exist');
+    });
+
     it('Deve selecionar um produto da lista', () => {
-   cy.get('.product-block')     //seleciona todos os produtos da página
-     //.first()  // busca o primeiro na fila
-     //last()   // busca o último na fila
-    .eq(2)   // busca 3º na fila
-    //.contains('Abominable Hoodie')  // busca o nome exato
-    .click()
-//cy.get('.quantity').should('exist')
-//cy.get('#tab-description > :nth-child(1)').should('exist')
-//cy.get('#tab-description > :nth-child(1)').should('contain' , 'It took')
-
-
+      produtosPage.buscarProdutoLista('Ariel Roll Sleeve Sweatshirt');
+    });
+    
+    it('Deve adicionar um produto ao carrinho', () => {
     });
 })
+
---
+++ cypress/support/page.objetcs/produtos.page.js
@@ -0,0 +1,30 @@
+class ProdutosPage {
+
+visitarUrl() {
+    cy.visit('produtos')
+}
+
+ buscarProduto(nomeProduto) {
+cy.get('[name="s"]').eq(1).type(nomeProduto)    //feito dessa forma porque n/ Cypress n/ é encontrado o campo de busca - foi necessário inspecionar a página
+cy.get('.search > .tbay-search-form > .form-ajax-search > .form-group > .input-group > .button-group > .button-search').click()
+//cy.get('.button-search').eq(1).click()          //forma simplificada quando há mais de um elemento com a mesma classe (inspecionando página)
+    }
+
+
+ buscarProdutoLista(nomeProduto) {
+cy.get('.products > .row ')
+.contains(nomeProduto)
+.click()
+
+ }
+
+ visitarProduto() {
+    //código a ser escrito
+ }
+
+ addProdutoCarrinho() {
+    //código a ser escrito
+ }
+
+}
+export default new ProdutosPage();     //precisa export ar a classe para usar em outros arquivos/ nos arquivos de teste precisa importar
