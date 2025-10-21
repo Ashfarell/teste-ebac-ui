bc5f43ee22d8f59901da8f377cb68890f8d55cd0
Ashfarell <ashfarell@gmail.com>
2025-10-20 22:26:09 -0300

    atualização teste

--- cypress/e2e/3-loja-ebac/produtos.cy.js
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -8,16 +8,15 @@ beforeEach(() => {
 });
 
     it('Deve selecionar um produto da lista', () => {
-        cy.get('.products > .row')
+   cy.get('.product-block')     //seleciona todos os produtos da página
      //.first()  // busca o primeiro na fila
-     //.last()   // busca o último na fila
-    //.eq(2)   // busca 3º na fila
-    .contains('Abominable Hoodie')  // busca o nome exato
-   .click()
-
-cy.get('.quantity').should('exist')
-cy.get('#tab-description > :nth-child(1)').should('exist')
-cy.get('#tab-description > :nth-child(1)').should('contain' , 'It took')
+     //last()   // busca o último na fila
+    .eq(2)   // busca 3º na fila
+    //.contains('Abominable Hoodie')  // busca o nome exato
+    .click()
+//cy.get('.quantity').should('exist')
+//cy.get('#tab-description > :nth-child(1)').should('exist')
+//cy.get('#tab-description > :nth-child(1)').should('contain' , 'It took')
 
 
     });
